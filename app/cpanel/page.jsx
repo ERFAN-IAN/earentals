5a1e8e0d0a8652d3ac80fa28efb3ff5dfb7c6ae9
Erfan <erfanaliyavari@gmail.com>
2024-04-23 15:40:11 +0330

    fixed admin

--- app/cpanel/page.jsx
+++ app/cpanel/page.jsx
@@ -1,12 +1,12 @@
-// import { headers } from "next/headers";
+import { headers } from "next/headers";
 const page = async () => {
-  // const response = await fetch(`http://localhost:3000/api/admin`, {
-  //   method: "GET",
-  //   // headers: headers(),
-  // });
-  // const tj = await response.json();
-  // const a = 0;
-  // console.log(tj);
+  const response = await fetch(`http://localhost:3000/api/admin`, {
+    method: "GET",
+    headers: headers(),
+  });
+  const tj = await response.json();
+  const a = 0;
+  console.log(tj);
   return (
     <div className="w-full">
       <div className="w-full grid grid-cols-2">
